d452425a25b72bf04090192d974993860c65f0da
Arthur Ghlijyan <deckuser@hotmail.com>
2020-11-24 14:37:18 +0400

    _

--- OpenClose/SmartWatch_1.cs
+++ OpenClose/SmartWatch_1.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenClose
+﻿namespace OpenClose
 {
     class SmartWatch_1 : ISmartWatch
     {
--- OpenClose/SmartWatch_2.cs
+++ OpenClose/SmartWatch_2.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenClose
+﻿namespace OpenClose
 {
     class SmartWatch_2 : ISmartWatch // SmartWatch_1
     {
